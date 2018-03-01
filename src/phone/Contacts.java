f7a149cf408cad46b102e3034c77e18964dcb918
Sean <seancurley007@gmail.com>
2018-03-01 14:49:37 -0800

    update

--- src/phone/Contacts.java
+++ src/phone/Contacts.java
@@ -1,9 +1,10 @@
 package phone;
+//Created by Sean C. on 3/1/2018
 
 public class Contacts{
-  private String name;
+    private String name;
   
-  public Contacts(String name){
-    this.name = name;
-  }
+    public Contacts(String name){
+        this.name = name;
+    }
 }
