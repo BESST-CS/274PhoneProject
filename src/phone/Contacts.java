839214491d74469ad4e0bca09b5de837fce4c6ba
ygvela <36971993+ygvela@users.noreply.github.com>
2018-03-06 14:22:59 -0800

    Update Contacts.java

--- src/phone/Contacts.java
+++ src/phone/Contacts.java
@@ -2,4 +2,21 @@ package phone;
 //Created by Sean C. on 3/1/2018
 
 public class Contacts{
+  
+  public Contacts(){
+    
+  }
+  
+  public void addContacts(){
+  
+  }
+  
+  public void deleteContact(){
+  
+  }
+  
+  public void editContact(){
+    
+  }
+   
 }
