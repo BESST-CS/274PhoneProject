bfd149c3c42673664ce5eb900e314c6d44f208d8
Sean <seancurley007@gmail.com>
2018-03-06 12:31:21 -0800

    Added Javadoc Comment

--- src/phone/Calls.java
+++ src/phone/Calls.java
@@ -2,4 +2,12 @@ package phone;
 //Created by Sean C. on 3/1/2018
 
 public class Calls {
+
+    public void makeCall(){
+
+    }
+
+    public void displayLog(){
+
+    }
 }
--- src/phone/Phonebook.java
+++ src/phone/Phonebook.java
@@ -1,5 +1,8 @@
 package phone;
 //Created by Sean C. on 3/1/2018
 
+/**
+ * Main class for project
+ */
 public class Phonebook {
 }
