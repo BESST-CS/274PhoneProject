fb6358d934d7123e4f3d7e4edd7542067ea3a18d
Yosselin <Yosselin@10.33.3.208>
2018-03-12 14:37:53 -0700

    Updates
    We have to add a displayContact() method and a compareTo() method in the
    Contact class.

--- src/edu/project2/Contacts.java
+++ src/edu/project2/Contacts.java
@@ -2,6 +2,7 @@ package edu.project2;
 //Created by Sean C. on 3/1/2018
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Handles a list of all the contacts.  Includes methods to add, delete, and modify contacts.  Handles creation of Contact objects
@@ -18,14 +19,42 @@ public class Contacts{
 
     public void addContact(Contact c){
         list.add(c);
+        Collections.sort(list);
+    }
+    
+    public void displayAll() {
+    	for(Contact c: list) {
+    		c.displayContact();
+    		System.out.println("\n");
+    	}
     }
 
-    public void deleteContact(){
-
+    public void editContact(int i){
+    	list.get(i);
     }
-
-    public void editContact(){
-
+    
+    public void deleteContact(int i){
+    	list.remove(i);
+    }
+    
+    public void addFavContact(Contact c) {
+    	favorites.add(c);
+    	Collections.sort(favorites);
+    }
+    
+    public void displayFavs() {
+    	for(Contact2 c: favorites) {
+    		c.displayContact();
+    		System.out.println("\n");
+    	}
+	}
+    
+    public void editFavContact(int i) {
+    	favorties.get(i);
+    }
+    
+    public void deleteFavContact(int i) {
+    	list.remove(i);
     }
 
 }
