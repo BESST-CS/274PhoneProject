019131166845c0547f347e5e16be280eb9fffb8f
Sean <seancurley007@gmail.com>
2018-03-12 19:09:29 -0700

    Lots of additions to Calls, Contact, and added 4 photos to use on favorites

--- src/edu/project2/Contact.java
+++ src/edu/project2/Contact.java
@@ -1,40 +1,44 @@
 package edu.project2;
 //Created by Sean C. on 3/8/2018
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Contains data for a contact including name, number, email, and notes.
  */
-public class Contact {
+public class Contact implements Comparable{
 
     private String name;
     private String number;
     private String email;
-    private ArrayList<String> notes;
+    private String note;
+    private FavoriteContactFrame picture;
 
-    public Contact(String name, String number, String email){
+    public Contact(String name, String number, String email, String note){
         this.name = name;
         this.number = number;
         this.email = email;
-        notes = new ArrayList<>();
+        this.note = note;
+        picture = null;
     }
 
-    public void setName(String name){
+    public Contact(String name, String number, String email, String note, String directory){
         this.name = name;
-    }
-
-    public void setNumber(String number){
         this.number = number;
-    }
-
-    public void setEmail(String email){
         this.email = email;
+        this.note = note;
+        this.picture = new FavoriteContactFrame(directory, 400, 400, name, number, email, note);
     }
 
-    public void addNote(String note){
-        notes.add(note);
-    }
+    public void setName(String name){this.name = name;}
+
+    public void setNumber(String number){this.number = number;}
+
+    public void setEmail(String email){this.email = email;}
+
+    public void setNote(String note){this.note = note;}
 
     public String getNumber(){return this.number;}
 
@@ -42,13 +46,50 @@ public class Contact {
 
     public String getEmail(){return this.email;}
 
-    public void displayNotes(){
-        for(String i : notes){
-            System.out.println("• "+i);
+    public void displayNotes(){System.out.println(note);}
+
+    public void displayContact() {
+        if(picture != null){
+            picture.displayContactFrame();
+        }else{
+            System.out.printf("Name: %s\nNumber: %s\nEmail: %s\nNote: %s\n", name, number, email, note);
         }
     }
 
     public boolean isEqual(Contact other){
         return other.getNumber().equals(this.number);
+    }
+
+    public String toString(){return name;}
+
+    @Override
+    public int compareTo(Object o) {
+        if(o.getClass().isAssignableFrom(Contact.class)) {
+            return ((Contact) o).getName().compareTo(this.getName());
+        }
+        return 0;
+    }
+
+    public static void editContact(Contact c){
+        Scanner k  = new Scanner(System.in);
+        System.out.println("What would you like to modify?\n" +
+                           "1: Name\n2: Number\n3: Email\n4: Note\n0: Done\n>> ");
+        int select = k.nextInt();
+        switch(select){
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 0:
+                break;
+            default:
+                break;
+
+        }
     }
 }
